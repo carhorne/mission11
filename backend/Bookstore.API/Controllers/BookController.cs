8c2a231224f3c37310cf354142921439ada60d46
carhorne <153124503+carhorne@users.noreply.github.com>
2025-03-25 15:16:27 -0600

    inital commit

---
+++ backend/Bookstore.API/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Bookstore.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        [HttpGet]
+        public IEnumerable <Book> GetBooks()
+        {
+            return _bookContext.Books.ToList();
+        }
+    }
+}
