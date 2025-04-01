e76861c177fb4f0746c18c08c3f72070e2355023
carhorne <153124503+carhorne@users.noreply.github.com>
2025-03-31 23:16:22 -0600

    fianl fianl tweaks

--- backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
+++ backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94338d70f6360a27eb9f7905abb74473a39fb244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d131cfa778b378b9463903452e7309b2ceae8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
