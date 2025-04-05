8d5accdb1f7b6a26e8d3553a9c1b8fdfc6b48ed5
carhorne <153124503+carhorne@users.noreply.github.com>
2025-04-04 19:57:28 -0600

    getting ready to deploy

--- backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
+++ backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d131cfa778b378b9463903452e7309b2ceae8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20377d74f4aeaceb9f527c7c68433bfa72f11a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
